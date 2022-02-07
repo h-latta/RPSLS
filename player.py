3dc8123991bd6d13c78393b96278f254b6ec256c
rahk1990 <rahkem.ellis@gmail.com>
2022-02-07 15:38:20 -0500

    Updated files names, added class to player super class

--- player.py
+++ player.py
@@ -1,3 +1,14 @@
 # ability to win or lose
 # keep record of wins
 # hand that throws the gesture
+
+class Player:
+
+    def __init__(self):
+        self.wins = 0
+        self.name = 'Jim'
+        self.chosen_gesture = "paper"
+        self.gestures_list = []
+
+    def choose_gesture():
+        pass
